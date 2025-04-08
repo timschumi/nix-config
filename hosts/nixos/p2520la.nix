da63d57b5eca10d1b2a51bf2a88feb5604d6fff3
Tim Schumacher <timschumi@gmx.de>
2025-04-09 01:34:11 +0200

    hosts/p2520la: Full rework

--- hosts/nixos/p2520la.nix
+++ hosts/nixos/p2520la.nix
@@ -4,11 +4,22 @@
   modules = [
     (inputs.self + "/fragments/comma.nix")
     (inputs.self + "/fragments/discord.nix")
+    (inputs.self + "/fragments/docker.nix")
+    (import (inputs.self + "/fragments/emulate.nix") [
+      "aarch64-linux"
+    ])
     (inputs.self + "/fragments/firefox.nix")
+    (inputs.self + "/fragments/libvirt.nix")
+    (inputs.self + "/fragments/lutris.nix")
+    (inputs.self + "/fragments/opentabletdriver.nix")
     (inputs.self + "/fragments/plasma.nix")
     (inputs.self + "/fragments/pipewire.nix")
+    (inputs.self + "/fragments/printing.nix")
     (inputs.self + "/fragments/spotify.nix")
+    (inputs.self + "/fragments/steam.nix")
+    (inputs.self + "/fragments/thunderbird.nix")
     (inputs.self + "/fragments/variant-desktop.nix")
+    (inputs.self + "/fragments/yubikey.nix")
 
     (
       { ... }:
@@ -18,6 +29,16 @@
             user = {
               tim = {
                 enable = true;
+                roles = [
+                  "android"
+                  "cad"
+                  "dev-cpp"
+                  "dev-dotnet"
+                  "dev-java"
+                  "dev-php"
+                  "dev-py"
+                  "dev-rust"
+                ];
               };
             };
           };
@@ -51,13 +72,19 @@
         boot.extraModulePackages = [ ];
 
         fileSystems."/" = {
-          device = "/dev/disk/by-uuid/d947f459-04f0-46fb-9476-dc3876eb36ee";
+          device = "/dev/disk/by-uuid/e52285cc-2db2-4a6b-8064-75ca79f1058a";
           fsType = "ext4";
         };
 
+        boot.initrd.luks.devices."root".device = "/dev/disk/by-uuid/51fe88b8-a5e9-434c-b171-2b06b21e6699";
+
         fileSystems."/boot" = {
-          device = "/dev/disk/by-uuid/8814-D120";
+          device = "/dev/disk/by-uuid/4A40-5EA2";
           fsType = "vfat";
+          options = [
+            "fmask=0077"
+            "dmask=0077"
+          ];
         };
 
         swapDevices = [ ];
@@ -69,7 +96,6 @@
         boot.loader.efi.canTouchEfiVariables = true;
 
         networking.hostName = "p2520la";
-        age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILlVgir2hj+ZfISiwmmSs7JMGgLF0UHxWKHvr0h8wP/I";
       }
     )
   ];
