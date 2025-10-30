bca6236a5c05a65fefbc785ec1b371d40b208a52
Tim Schumacher <timschumi@gmx.de>
2025-10-31 00:40:51 +0100

    Reformat files

--- modules/users/role/ctf.nix
+++ modules/users/role/ctf.nix
@@ -28,142 +28,146 @@ in
     };
 
     home-manager.users."${user}" = {
-      home.packages = with pkgs; [
-        aflplusplus
-        apktool
-        ascii
-        avalonia-ilspy
-        bettercap
-        binaryninja-personal
-        binwalk
-        biodiff
-        bkcrack
-        burpsuite
-        bytecode-viewer
-        capstone
-        checksec
-        coreboot-utils
-        (cutter.withPlugins (
-          p: with p; [
-            jsdec
-            rz-ghidra
-            sigdb
-          ]
-        ))
-        delsum
-        detect-it-easy
-        dex2jar
-        diffoscope
-        dig
-        dnscrypt-proxy
-        elfutils
-        ettercap
-        exiftool
-        exploitdb
-        ffmpeg-full
-        flashrom
-        freerdp
-        frida-tools
-        fuzzdb
-        gdb
-        ghex
-        (ghidra.withExtensions (
-          p: with p; [
-            findcrypt
-            ghidra-delinker-extension
-            ghidra-firmware-utils
-            ghidra-golanganalyzerextension
-            kaiju
-            ret-sync
-            wasm
-          ]
-          ++ optionals (!config.nixpkgs.config.contentAddressedByDefault) (
-            with p;
-            [
-              # Something, something, "heat death of the universe" (via swift).
-              ghidraninja-ghidra-scripts
-            ]
-          )
-        ))
-        hashcat
-        hashcat-utils
-        honggfuzz
-        hopper
-        httptunnel
-        iaito
-        ida-free
-        imagemagick
-        imhex
-        insomnia
-        jadx
-        jq
-        lldb
-        loadlibrary
-        ltrace
-        minimodem
-        mitmproxy
-        nasm
-        nmap
-        ngrok
-        packer
-        pahole
-        payloadsallthethings
-        proxychains-ng
-        (python3.withPackages (
-          p: with p; [
-            angr
-            ropper
-            standard-telnetlib
-          ]
-        ))
-        radare2
-        # CMake 3.5 failure
-        #retdec
-        rizin
-        ropgadget
-        scanmem
-        seclists
-        shadowsocks-rust
-        shadowsocks-v2ray-plugin
-        sleuthkit
-        socat
-        strace
-        tcpdump
-        traceroute
-        uefitool
-        udp2raw
-        udptunnel
-        unrar
-        upx
-        usbtop
-        v2ray
-        valgrind
-        vbindiff
-        vim.xxd
-        vivisect
-        volatility2-bin
-        volatility3
-        wget
-        whois
-        winetricks
-        wineWowPackages.staging
-        wireguard-tools
-        wireshark
-        wstunnel
-        xournalpp
-        yt-dlp
-      ]
-      ++ optionals (!config.nixpkgs.config.contentAddressedByDefault) (
+      home.packages =
         with pkgs;
         [
-          # Something, something, "heat death of the universe".
-          chromium
-          # Something, something, "heat death of the universe" (via electron).
-          drawio
-          # Conflicts with itself for whatever reason.
-          file
+          aflplusplus
+          apktool
+          ascii
+          avalonia-ilspy
+          bettercap
+          binaryninja-personal
+          binwalk
+          biodiff
+          bkcrack
+          burpsuite
+          bytecode-viewer
+          capstone
+          checksec
+          coreboot-utils
+          (cutter.withPlugins (
+            p: with p; [
+              jsdec
+              rz-ghidra
+              sigdb
+            ]
+          ))
+          delsum
+          detect-it-easy
+          dex2jar
+          diffoscope
+          dig
+          dnscrypt-proxy
+          elfutils
+          ettercap
+          exiftool
+          exploitdb
+          ffmpeg-full
+          flashrom
+          freerdp
+          frida-tools
+          fuzzdb
+          gdb
+          ghex
+          (ghidra.withExtensions (
+            p:
+            with p;
+            [
+              findcrypt
+              ghidra-delinker-extension
+              ghidra-firmware-utils
+              ghidra-golanganalyzerextension
+              kaiju
+              ret-sync
+              wasm
+            ]
+            ++ optionals (!config.nixpkgs.config.contentAddressedByDefault) (
+              with p;
+              [
+                # Something, something, "heat death of the universe" (via swift).
+                ghidraninja-ghidra-scripts
+              ]
+            )
+          ))
+          hashcat
+          hashcat-utils
+          honggfuzz
+          hopper
+          httptunnel
+          iaito
+          ida-free
+          imagemagick
+          imhex
+          insomnia
+          jadx
+          jq
+          lldb
+          loadlibrary
+          ltrace
+          minimodem
+          mitmproxy
+          nasm
+          nmap
+          ngrok
+          packer
+          pahole
+          payloadsallthethings
+          proxychains-ng
+          (python3.withPackages (
+            p: with p; [
+              angr
+              ropper
+              standard-telnetlib
+            ]
+          ))
+          radare2
+          # CMake 3.5 failure
+          #retdec
+          rizin
+          ropgadget
+          scanmem
+          seclists
+          shadowsocks-rust
+          shadowsocks-v2ray-plugin
+          sleuthkit
+          socat
+          strace
+          tcpdump
+          traceroute
+          uefitool
+          udp2raw
+          udptunnel
+          unrar
+          upx
+          usbtop
+          v2ray
+          valgrind
+          vbindiff
+          vim.xxd
+          vivisect
+          volatility2-bin
+          volatility3
+          wget
+          whois
+          winetricks
+          wineWowPackages.staging
+          wireguard-tools
+          wireshark
+          wstunnel
+          xournalpp
+          yt-dlp
         ]
-      );
+        ++ optionals (!config.nixpkgs.config.contentAddressedByDefault) (
+          with pkgs;
+          [
+            # Something, something, "heat death of the universe".
+            chromium
+            # Something, something, "heat death of the universe" (via electron).
+            drawio
+            # Conflicts with itself for whatever reason.
+            file
+          ]
+        );
 
       home.extraDependencies = with pkgs; [
         binaryninja-free
