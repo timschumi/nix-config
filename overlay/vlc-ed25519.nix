6754dba9201bc24ecb23d683c06b5bba0985154b
Tim Schumacher <timschumi@gmx.de>
2026-01-25 12:12:32 +0100

    More VLC pubkey bugfixes

--- overlay/vlc-ed25519.nix
+++ overlay/vlc-ed25519.nix
@@ -13,6 +13,11 @@
             url = "https://github.com/videolan/vlc/commit/6368db7b66414ce73db066c373021f1706113dee.patch?full_index=1";
             hash = "sha256-eEsoHRWNtjt/KqajMU+Ld9cRLJlxFpqAHWjanB3ohdU=";
           })
+          # access: sftp: store creds on successful pubkey auth
+          (final.fetchpatch2 {
+            url = "https://github.com/videolan/vlc/commit/dcc36360256dfa11bf57880c063ca02d8bdf4d90.patch?full_index=1";
+            hash = "sha256-g7nTwYsoejfYusElsxod5d485Q2q94ON7HKMmGw9/Go=";
+          })
         ];
       }
     );
