876a707ae7f7a57058917274cab1db7a7a3af9c0
Tim Schumacher <timschumi@gmx.de>
2026-03-22 19:27:24 +0100

    role/android: Emulate android-udev-rules for remote access

--- modules/users/role/android.nix
+++ modules/users/role/android.nix
@@ -16,6 +16,7 @@ let
 in
 {
   config = mkIf (elem role config.extra.user."${user}".roles) {
+    users.groups.adbusers = { };
     users.users."${user}".extraGroups = [
       "adbusers"
       "dialout"
@@ -37,5 +38,11 @@ in
     services.udev.packages = with pkgs; [
       mtkclient
     ];
+
+    services.udev.extraRules = ''
+      # ID_DEBUG_APPLIANCE is set up by systemd's 70-uaccess.rules. We can rely on this because we are 99-local.rules.
+      # Emulate the old android-udev-rules behavior for those, since uaccess does not work for remote sessions.
+      ENV{ID_DEBUG_APPLIANCE}=="?*", MODE="0660", GROUP="adbusers", SYMLINK+="android", SYMLINK+="android%n"
+    '';
   };
 }
